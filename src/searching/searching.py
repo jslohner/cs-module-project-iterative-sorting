6ac112a013138ad6c7583599b3534905cd8bb6c8
Justin Lohner <lohner4@gmail.com>
2020-07-20 14:03:39 -0600

    wrote linear_search function

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,14 @@
 def linear_search(arr, target):
-    # Your code here
-
-
-    return -1   # not found
+	for i in range(0, len(arr)):
+		if arr[i] == target:
+			return i
+	return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
+	# Your code here
 
 
-    return -1  # not found
+	return -1  # not found
